c9c1a647c6954d1710585c2772d625bbab9bec55
Ron Soeum <ronsoeum@gmail.com>
2019-01-28 10:30:44 -0600

    review and tested exercise, minor change

--- albums_migration.sql
+++ albums_migration.sql
@@ -4,7 +4,7 @@ DROP TABLE IF EXISTS albums;
 
 CREATE TABLE albums (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    artist VARCHAR(100) DEFAULT 'NONE',
+    artist VARCHAR(100) NOT NULL,
     name  VARCHAR(100) NOT NULL,
     release_date DATE,
     sales FLOAT,
