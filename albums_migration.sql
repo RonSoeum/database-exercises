0d966e534bc25e61e2a38b0df5c4287b5569a397
Ron Soeum <ronsoeum@gmail.com>
2019-01-28 11:55:17 -0600

    MySQL: Basic Statements - created albums_seeder.sql and added records to the albums table

--- albums_migration.sql
+++ albums_migration.sql
@@ -6,8 +6,20 @@ CREATE TABLE albums (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     artist VARCHAR(100) NOT NULL,
     name  VARCHAR(100) NOT NULL,
-    release_date DATE,
+    release_date INT,
     sales FLOAT,
     genre VARCHAR(50) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+-- ==============================================
+
+DROP TABLE IF EXISTS quotes;
+
+CREATE TABLE quotes (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    author_first_name VARCHAR(50) DEFAULT 'NONE',
+    author_last_name  VARCHAR(100) NOT NULL,
+    content TEXT NOT NULL,
     PRIMARY KEY (id)
 );
