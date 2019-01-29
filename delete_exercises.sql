b2a337e1b717faaa7eb0dc1728241ae421f5b1c6
Ron Soeum <ronsoeum@gmail.com>
2019-01-28 22:30:36 -0600

    fixed captioning

--- delete_exercises.sql
+++ delete_exercises.sql
@@ -9,15 +9,32 @@ USE codeup_test_db;
 -- Make sure to put appropriate captions before each SELECT.
 -- Convert the SELECT statements to DELETE.
 
-SELECT 'Albums released after 1991.' AS 'Info';
-DELETE from albums WHERE release_date > 1991;
+SELECT name AS 'Albums released after 1991.', release_date
+FROM albums
+WHERE release_date > 1991;
 
+DELETE from albums
+WHERE release_date > 1991;
 
+--
 
-SELECT 'Albums with the genre ''disco''.' AS 'Info';
-DELETE from albums WHERE genre = 'disco';
+SELECT name AS 'Albums with the genre ''disco''.', genre
+FROM albums
+WHERE genre = 'disco';
 
+DELETE from albums
+WHERE genre = 'disco';
 
+--
 
-SELECT 'Albums by ''Whitney Houston''.' AS 'Info';
-DELETE from albums WHERE artist = 'Whitney Houston';
+SELECT artist, name AS 'Albums by ''Led Zeppelin''.'
+FROM albums
+WHERE artist = 'Led Zeppelin';
+
+DELETE from albums
+WHERE artist = 'Led Zeppelin';
+
+--
+
+SELECT artist, name AS 'All Albums left after delete exercise.', release_date, sales, genre
+FROM albums
