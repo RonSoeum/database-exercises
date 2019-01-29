3f862bca7c08343c8159a7959e8bedb7bffedc97
Ron Soeum <ronsoeum@gmail.com>
2019-01-28 22:14:12 -0600

    fixed captioning

--- update_exercises.sql
+++ update_exercises.sql
@@ -13,22 +13,31 @@ USE codeup_test_db;
 -- Move all the albums before 1980 back to the 1800s.
 -- Change 'Michael Jackson' to 'Peter Jackson'
 
-SELECT 'All albums in your table.' AS 'Info';
-SELECT * FROM albums;
-UPDATE albums SET sales = (sales * 10);
-SELECT 'Updated: All albums sales to be 10 times more popular.' AS 'Info';
-SELECT * FROM albums;
+SELECT name AS 'All albums in your table.', sales
+FROM albums;
 
+UPDATE albums
+SET sales = (sales * 10);
 
-SELECT 'All albums released before 1980.' AS 'Info';
-SELECT * FROM albums where release_date < 1980;
+SELECT name AS 'Updated: All albums sales to be 10 times more popular.', sales
+FROM albums;
+
+--
+
+SELECT name AS 'All albums released before 1980.', release_date
+FROM albums where release_date < 1980;
+
 UPDATE albums SET release_date = 1800 WHERE release_date < 1980;
-SELECT 'Updated: All albums released before 1980 to 1800.' AS 'Info';
-SELECT * FROM albums where release_date = 1800;
 
+SELECT name AS 'Updated: All albums released before 1980 changed to 1800.', release_date
+FROM albums where release_date = 1800;
 
-SELECT 'All albums by Michael Jackson.' AS 'Info';
-SELECT * FROM albums where artist = 'Michael Jackson';
+--
+
+SELECT artist, name AS 'All albums by Michael Jackson.'
+FROM albums where artist = 'Michael Jackson';
+
 UPDATE albums SET artist = 'Peter Jackson' WHERE artist = 'Michael Jackson';
-SELECT 'Updated: All albums by Michael Jackson to Peter Jackcon.' AS 'Info';
-SELECT * FROM albums where artist = 'Peter Jackson';
+
+SELECT artist, name AS 'Updated: All albums by Michael Jackson changed to Peter Jackcon.'
+FROM albums where artist = 'Peter Jackson';
